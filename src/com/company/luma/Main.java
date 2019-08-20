ff9be4e4c81d034c2ec2eb03cff0fe9ef830c9d8
Lu Ma <luma26c@gmail.com>
2019-08-20 09:35:01 -0700

    change to not today

--- src/com/company/luma/Main.java
+++ src/com/company/luma/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("not today");
         System.out.println("hello world 123456");
     }
 }
