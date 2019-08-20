4076a67314b2679948401a04010d6f3f5e75369f
Lu Ma <luma26c@gmail.com>
2019-08-20 08:38:23 -0700

    change 6

--- src/com/company/luma/Main.java
+++ src/com/company/luma/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello world 12345");
+        System.out.println("hello world 123456");
     }
 }
